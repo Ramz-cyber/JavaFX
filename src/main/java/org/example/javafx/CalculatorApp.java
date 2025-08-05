fd941f1eb6cd4a0596b2ea28997cf7431064713e
Ramz-cyber <sodiqsonr@gmail.com>
2025-08-05 09:36:48 +0100

    first commit

---
+++ src/main/java/org/example/javafx/CalculatorApp.java
@@ -0,0 +1,11 @@
+package org.example.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalculatorApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
